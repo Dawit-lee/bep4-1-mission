18bfa55443532343f200593405f8fee7ba99c7b6
sik2 <bickwang123@gmail.com>
2025-12-23 00:18:56 +0900

    0001

--- src/main/java/com/back/entity/Member.java
+++ src/main/java/com/back/entity/Member.java
@@ -1,4 +1,24 @@
 package com.back.entity;
 
-public class Member {
+import com.back.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Member extends BaseIdAndTime {
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    public Member(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
