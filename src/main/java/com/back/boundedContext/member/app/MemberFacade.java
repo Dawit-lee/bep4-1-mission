465c8a934283736ad342b9d3928803b7af51df79
Dawit-lee <edw1372@naver.com>
2026-09-18 10:22:36 +0900

    0013 : 랜덤 팁 제공

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,39 +1,44 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.domain.MemberPolicy;
 import com.back.boundedContext.member.out.MemberRepository;
-import com.back.global.exception.DomainException;
 import com.back.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberFacade {
     private final MemberRepository memberRepository;
+    private final MemberJoinUseCase memberJoinUseCase;
+    private final MemberPolicy memberPolicy;
 
-    public MemberFacade(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
+    @Transactional(readOnly = true)
     public long count() {
         return memberRepository.count();
     }
 
+    @Transactional
     public RsData<Member> join(String username, String password, String nickname) {
-        findByUsername(username).ifPresent(m -> {
-            throw new DomainException("409-1", "이미 존재하는 username 입니다.");
-        });
-
-        Member member = memberRepository.save(new Member(username, password, nickname));
-
-        return new RsData<>("201-1", "%d번 회원이 생성되었습니다.".formatted(member.getId()), member);
+        return memberJoinUseCase.join(username, password, nickname);
     }
 
+    @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
+
+    @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip() {
+        return "비밀번호의 유효기간은 %d일 입니다."
+                .formatted(memberPolicy.getNeedToChangePasswordDays());
     }
 }
--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,9 +1,13 @@
 package com.back.boundedContext.post.app;
 
+import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.post.dto.PostDto;
+import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +17,25 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PostWriteUseCase {
 	private final PostRepository postRepository;
-
-	public long count() {
-		return postRepository.count();
-	}
-
-	public Optional<Post> findById(int id) {
-		return postRepository.findById(id);
-	}
+	private final EventPublisher eventPublisher;
+	private final MemberFacade memberFacade;
 
 	public RsData<Post> write(Member author, String title, String content) {
-		Post post = new Post(author, title, content);
+		Post post = postRepository.save(new Post(author, title, content));
 
-		//TODO : 이벤트 수정
-		author.increaseActivityScore(3);
+		eventPublisher.publish(
+				new PostCreatedEvent(
+						new PostDto(post)
+				)
+		);
 
-		return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
+		String randomSecureTip = memberFacade.getRandomSecureTip();
+
+		return new RsData<>(
+				"201-1",
+				"%d번 글이 생성되었습니다. 보안 팁 : %s"
+						.formatted(post.getId(), randomSecureTip),
+				post
+		);
 	}
 }
